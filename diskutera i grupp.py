c31e869d46fd5f07b7ba95dcaa639b2818141a19
majk9494 <majkmajk420420@gmail.com>
2026-01-16 00:29:23 +0100

    first commit

---
+++ diskutera i grupp.py
@@ -0,0 +1,9 @@
+# 2 Diskutera i grupp
+ticket_price = 100
+money_in_pocket = 200
+
+money_left = money_in_pocket - ticket_price
+print("Det blir " + str(money_left) + " kronor över.")
+
+per_person = money_left / 2
+print("Varje person får " + str(per_person) + " kronor.")
